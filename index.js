b39e4d32faf7d8ef94fa7aec5fb4bc750a02d74f
Benji Bogush <70986668+benjibogush@users.noreply.github.com>
2022-03-23 20:58:37 +0000

    delete prohibited words

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const port = 4444;
 
@@ -27,13 +28,39 @@ client.on('message', msg => {
   if(msg.content === 'beep'){
     msg.reply('boop!')
   }
-  if(msg.content === 'is here.'){
+  if(msg.content === 'is here'){
     // msg.reply('Welcome to life boat!');
     msg.reply(`Hey  ${msg.author}, welcome to life boat! Thanks for joining.`)
   }
 });
 
+client.on("messageDelete", function(message){
+    console.log(`message is deleted -> ${message}`);
+});
 
+client.on("messageDeleteBulk", function(messages){
+    console.log(`messages are deleted -> ${messages}`);
+});
+
+client.on('messageCreate', (message) => {
+    if(message.content.toLowerCase().includes('hey bot') || message.content.toLowerCase().includes('general kenobi')){
+        message.channel.send('Hello there!');
+    }
+});
+
+client.on('messageCreate', (message) => {
+    if(message.content.toLowerCase().includes('fudge') || message.content.toLowerCase().includes('pudding')){
+        message.channel.send('Such language is prohibited!');
+      message.delete();
+    }
+});
+
+
+
+
+client.on("error", function(error){
+    console.error(`client's WebSocket encountered a connection error: ${error}`);
+});
 
 // Login to Discord with your client's token
 client.login(process.env.token);
