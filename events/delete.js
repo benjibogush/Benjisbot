9558d6356a706e148cc046beecc1cd0ac43fbc1e
Benji Bogush <70986668+benjibogush@users.noreply.github.com>
2022-04-21 04:17:28 +0000

    added profanity filter in delete.js

--- events/delete.js
+++ events/delete.js
@@ -1,45 +1,53 @@
-
+const Profanity = require('profanity-js');
 
 
 module.exports = (client) => {
-   client.on('messageCreate', (message) => {
-        if(message.content.toLowerCase().includes('fudge') || message.content.toLowerCase().includes('pudding')){
-            message.channel.send('Such language is prohibited!');
-            message.delete();
-          
-        }
-    });
+  const list = []
 
-client.on("messageDelete", function(message){
-    console.log(`message is deleted -> ${message}`);
-});
+  client.on('messageCreate', (message) => {
+    const profanity = new Profanity()
+    
+    if (message.content.toLowerCase().includes('fudge') || message.content.toLowerCase().includes('pudding')) {
+      message.channel.send('Such language is prohibited!');
+      message.delete();
+    }
 
-client.on("messageDeleteBulk", async messages => {
+    if(profanity.isProfane(message.content.toLowerCase())){
+       message.channel.send('Such language is prohibited!');
+       message.delete();
+    }
+  });
+
+
+
+  client.on("messageDeleteBulk", async messages => {
     //  msg.delete(text);
-      const length = messages.array().length;
-      const channel = messages.first().channel.name;
+    const length = messages.array().length;
+    const channel = messages.first().channel.name;
 
-      const embed = new Discord.MessageEmbed()
-          .setTitle(`${length} Messages purged in #${channel}`)
-          .setDescription(messages.map(message =>         `[${message.author.tag}]: ${message.content}`))
-          .setFooter(`${length} latest shown`)
-          .setColor('#dd5f53')
-          .setTimestamp();
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`${length} Messages purged in #${channel}`)
+      .setDescription(messages.map(message => `[${message.author.tag}]: ${message.content}`))
+      .setFooter(`${length} latest shown`)
+      .setColor('#dd5f53')
+      .setTimestamp();
 
     // use this to send the message to the channel the bulk delete     happened in 
-  messages.first().channel.send(embed);
-  // alternatively, use this to send the message to a specific channel
-//  (await client.channels.fetch(/* channel ID */)).send(embed);
-    
-});
+    messages.first().channel.send(embed);
+    // alternatively, use this to send the message to a specific channel
+    //  (await client.channels.fetch(/* channel ID */)).send(embed);
 
-    
-  
-client.on("messageDeleteBulk", function(messages){
-      console.log(`messages are deleted -> ${messages}`);
-});
+  });
 
-  
+
+
+  // client.on("messageDeleteBulk", function(messages) {
+  //   console.log(`messages are deleted -> ${messages}`);
+  // });
+
+  // client.on("messageDelete", function(message) {
+  //   console.log(`message is deleted -> ${message}`);
+  // });
 
 
 
--- events/sendImage.js
+++ events/sendImage.js
@@ -2,33 +2,35 @@ const fs = require('fs')
 
 module.exports = (client) => {
 
-// function getPath(path) {
-//      // const path ='./public/img/tech/'
-//     const files = fs.readdirSync(path)
-//     const randomIndex = Math.floor(Math.random() * files.length)
-//     const file = files[randomIndex]
-//   return file;
-// } 
 
-client.on('messageCreate', (message) => {
-    const path ='./public/img/tech/'
-  const pathAnimal = './public/img/animal/'
-    
-    const files = fs.readdirSync(path)
-    const randomIndex = Math.floor(Math.random() * files.length)
-    const file = files[randomIndex]
 
-  if(message.content.toLowerCase().includes('tech')){
-     message.channel.send({ files:[{attachment: path+file}] })
-  }
+    // function sendMessage(key, path, fileIndex) {
+    //  if(message.content.toLowerCase().includes(key)){
+    //      message.channel.send({ files:[{attachment: path+file}] })
+    //  }
+    // } 
 
-    
-   
-})
-          
+    client.on('messageCreate', (message) => {
+      const path = './public/img/tech/'
+      const pathAnimal = './public/img/animal/'
 
-    
+      const files = fs.readdirSync(path)
+      const randomIndex = Math.floor(Math.random() * files.length)
+      const file = files[randomIndex]
 
-    
+      if (message.content.toLowerCase() === ('tech')) {
+        message.channel.send({ files: [{ attachment: path + file }] })
+      }
+
+      // let key = message.content.toLowerCase()
+      // sendMessage(key, path, file)
+
+
+    })
+
+
+
+
+
   }
 
